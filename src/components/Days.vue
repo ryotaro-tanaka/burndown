7646188a1666fcffa2a70657947d6cc9205a1406
ryotaro tanaka <ryotaro_tanaka@outlook.com>
2019-12-19 21:55:51 +0900

    add memory

--- src/components/Days.vue
+++ src/components/Days.vue
@@ -4,8 +4,16 @@
         <div class="label"
         :style="{width: graphWidth + 'px'}">
             <div class="day"
+            :class="{nonWork: !day.isWork}"
             v-for="day in days" :key="day.val">
-                {{ day.val }}({{ day.isWork }})
+                {{ day.val }}
+            </div>
+        </div>
+        <div class="memory"
+        :style="{width: graphWidth + 'px', height: graphHeight + 'px'}">
+            <div class="day"
+            :class="{nonWork: !day.isWork, monday: day.weekId === 0}"
+            v-for="day in days" :key="'memory' + day.val">
             </div>
         </div>
     </div>
@@ -31,7 +39,8 @@ export default {
 
                 days.push({
                     val: `${day.getMonth() + 1}/${day.getDate()}`,
-                    isWork: (week === undefined)? false: week.is_work === 1
+                    isWork: (week === undefined)? false: week.is_work === 1,
+                    weekId: (week === undefined)? -1: week.week_id
                 });
             }
 
@@ -61,7 +70,7 @@ export default {
     position: absolute;
     top: 0;
     left: 0;
-    pointer-events: none;
+    // pointer-events: none;
     .label {
         position: sticky;
         display: flex;
@@ -69,11 +78,36 @@ export default {
         top: 0;
         left: 0;
         margin-left: $headspace-width + $tasks-width;
+        background-color: $frame-color;
+        color: $frame-char-color;
         .day {
-            border-bottom: medium solid black;
-            border-right: thin solid gray;
+            border-bottom: thin solid $frame-char-color;
             width: $days-width;
             min-width: $days-width;
+            overflow: hidden;
+        }
+        .nonWork {
+            color: rgba($color: $frame-char-color, $alpha: 0.5);
+        }
+    }
+    .memory {
+        position: absolute;
+        display: flex;
+        top: $headspace-height;
+        left: $headspace-width + $tasks-width;
+        background: transparent;
+        z-index: -1;
+        .day {
+            border-right: thin solid $frame-char-color;
+            width: $days-width;
+            min-width: $days-width;
+            overflow: hidden;
+        }
+        .nonWork {
+            background-color: rgba($color: $frame-char-color, $alpha: 0.1);
+        }
+        .monday {
+            border-left: thin solid $frame-char-color;
         }
     }
 }
